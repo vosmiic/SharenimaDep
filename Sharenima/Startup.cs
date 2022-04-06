aab211489b7b335f143ebbde1a489f1751e8d724
Patrick <44092756+vigetious@users.noreply.github.com>
2022-04-06 16:41:10 +0100

    moving back to dotnet 5 startup and main

---
+++ Sharenima/Startup.cs
@@ -0,0 +1,140 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Sharenima.Data;
+using Sharenima.Models;
+
+namespace Sharenima; 
+
+public class Startup {
+    public static IConfiguration Configuration { get; private set; }
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection serviceCollection) {
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(connectionString));
+        serviceCollection.AddDbContext<MainContext>();
+        serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
+
+        serviceCollection.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        serviceCollection.AddIdentityServer()
+            .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        serviceCollection.AddAuthentication()
+            .AddIdentityServerJwt();
+
+        serviceCollection.TryAddEnumerable(
+            ServiceDescriptor.Singleton<IPostConfigureOptions<JwtBearerOptions>,
+                ConfigureJwtBearerOptions>());
+
+        serviceCollection.AddControllersWithViews();
+        serviceCollection.AddRazorPages();
+        serviceCollection.AddCors(o => o.AddPolicy("CorsPolicy", builder => {
+            builder.WithOrigins("https://localhost:44484")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        }));
+
+        serviceCollection.AddSignalR(options => { options.EnableDetailedErrors = true; });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if (env.IsDevelopment()) {
+            app.UseMigrationsEndPoint();
+        } else {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.UseCors("CorsPolicy");
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapRazorPages();
+            endpoints.MapHub<SignalRHub>("/hub");
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
+//var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+/*var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDbContext<MainContext>();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+builder.Services.AddAuthentication()
+    .AddIdentityServerJwt();
+
+builder.Services.TryAddEnumerable(
+    ServiceDescriptor.Singleton<IPostConfigureOptions<JwtBearerOptions>,
+        ConfigureJwtBearerOptions>());
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder => {
+    builder.WithOrigins("https://localhost:44484")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+}));
+
+builder.Services.AddSignalR(options => { options.EnableDetailedErrors = true; });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseMigrationsEndPoint();
+} else {
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
+
+app.MapHub<SignalRHub>("/hub");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.MapFallbackToFile("index.html");
+;
+
+app.Run();*/
